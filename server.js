0faabb0f861dab9745c8429a90bf5df200bd1040
João Haskel <johaskel@gmail.com>
2022-08-09 16:34:35 -0300

    server

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
+
 const logger = require('morgan');
 const cors = require('cors');
+const debug = require('debug')('nodestr:server');
+const multer = require("multer");
 
 
 const usersRoutes= require('./routes/userRoutes');
 
 
 
-const port = process.env.PORT || 3000;
+
+const port = normalizePort( process.env.PORT || '3000');
+app.set('port',port);
+const server= http.createServer(app);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
@@ -23,20 +28,68 @@ app.set('port',port);
 
 usersRoutes(app);
 
-server.listen(3000, 'localhost',function(){
-    console.log('Aplication de NodeJs ' +process.pid+' iniciada')
-});
+process.env.TZ = "America/Sao_Paulo";
+server.listen(port);
+server.on('error',onError);
+server.on('listening',onListening);
 
-app.get('/',(req,res)=>{
-    res.send('Ruta raiz del backend2');
-});
-
-app.get('/compras',(req,res)=>{
-    res.send('Rota de compras');
-})
-//error handler
-
-app.use((err,req,res,next)=>{
+console.log('Api rodando na porta ' + port);
+// ERROR HANDLER
+app.use((err, req, res, next) => {
     console.log(err);
-    res.status(err.status || 500).send(err.stack)
-})
+    res.status(err.status || 500).send(err.stack);
+});
+
+function normalizePort(val){
+
+    const port = parseInt(val,10);
+    if(isNaN(port)){
+        return val;
+    }
+    if(port >= 0){
+        return port;
+    }
+    return false;    
+}
+
+function onError(error){
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+    const bind = typeof port === 'string'?
+    'Pipe ' + port :
+    'Port ' + port;
+    switch(error.code){
+        case 'EACESS':
+            console.error(bind + ' requires elevated previlegios');
+            process.exit(1);
+            break;
+            case 'EADDRINUSE':
+                console.error(bind + ' IS READY USED');
+            process.exit(1);
+            break;
+
+           
+            default:
+                throw error;
+
+    }
+
+}
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string'?
+    'pipe ' + addr :
+    'port ' + addr.port;
+
+    debug('Listening on '+bind);
+
+
+}
+
+module.exports = {
+    app: app,
+    server:server
+}
+
+
